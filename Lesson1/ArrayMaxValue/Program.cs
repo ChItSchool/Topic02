29e0cd6f373392508eeeb42e854ee14d45130420
Slawik Iwanenko <good.worker.1997@gmail.com>
2018-01-19 12:54:15 +0200

    Add GetArrayMaxVal func

--- Lesson1/ArrayMaxValue/Program.cs
+++ Lesson1/ArrayMaxValue/Program.cs
@@ -10,6 +10,19 @@ namespace ArrayMaxValue
     {
         static void Main(string[] args)
         {
+            int[] Array = { 1, 2, 2, 3, 5, 8, 10 };
+
+        }
+
+        static int GetArrayMaxVal(int[] array)
+        {
+            int max = array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                    max = array[i];
+            }
+            return max;
         }
     }
 }
