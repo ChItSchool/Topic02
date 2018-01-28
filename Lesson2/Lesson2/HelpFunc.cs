9b9fdd3ebee66dee862be8265d2621f0cd6f86de
Slawik Iwanenko <good.worker.1997@gmail.com>
2018-01-28 09:15:42 +0200

    init Lesson2

---
+++ Lesson2/Lesson2/HelpFunc.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2
+{
+    /// <summary>
+    /// Надає деякі додаткові функції для роботи з масивом
+    /// </summary>
+    public static class HelpFunc
+    {
+        /// <summary>
+        /// Виводить в консоль всі значення змінних масива цілих чисел
+        /// </summary>
+        /// <param name="array"> Масив для виведення на екран </param>
+        /// <param name="arrayName"></param>
+        public static void PrintArray( int [] array, string arrayName = "" )
+        {
+            Console.WriteLine("Масив " + arrayName + ": ");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if ( i % 50 == 0 )
+                    Console.WriteLine();
+
+                Console.Write(array[i]);
+
+                if ( i+1 >= array.Length)
+                    Console.Write(", ");
+                else
+                    Console.Write(".");
+            }
+        }
+
+        /// <summary>
+        /// Повертає новий масив, з рандомними значеннями
+        /// </summary>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        public static int[] GetRandomArray(int length)
+        {
+            var array = new int[length];
+            Random random = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+                array[i] = random.Next();
+
+            return array;
+        }
+
+    }
+}
