a460e752c4ce636841f25729f67d78cb5a0d694b
Slawik Iwanenko <good.worker.1997@gmail.com>
2018-03-25 10:21:22 +0300

    add Notes

---
+++ Notes/Notes/Services/FileService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Services
+{
+    public class FileService
+    {
+        public FileService()
+        {
+
+        }
+
+        /// <summary>
+        /// Відкриває файл за вказаним шляхом
+        /// </summary>
+        /// <param name="path">
+        /// шлях до файлу в файловій системі
+        /// </param>
+        /// <returns>
+        /// Текст з даного файлу
+        /// </returns>
+        public string Open(string path)
+        {
+            string text = "";
+            return text;
+        }
+
+        /// <summary>
+        /// Створює новий  файл за вказаним шляхом
+        /// </summary>
+        /// <param name="path">шлях до папки де буде знаходитись новий файл </param>
+        /// /// <param name="name"> Ім'я нового файлу</param>
+        /// <returns>
+        /// true коли вдалося створити файл
+        /// false коли не вдалося створити файл
+        /// </returns>
+        public bool Create(string name,string path)
+        {
+            return false;
+        }
+
+        /// <summary>
+        /// Зберігає оновлене значення в файл, який було відкрито
+        /// </summary>
+        /// <param name="text">
+        /// значення яке потрібно записати
+        /// </param>
+        /// <returns>
+        /// true коли вдалося зберегти зміни в файл
+        /// false коли не вдалося зберегти зміни в файл
+        /// </returns>
+        public bool Save(string text)
+        {
+            return false;
+        }
+    }
+}
---
+++ Notes/Notes/Services/TextService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Services
+{
+    public class TextService
+    {
+
+        public TextService()
+        {
+                
+        }
+
+        /// <summary>
+        /// Повертає попереднє значення текстового поля
+        /// </summary>
+        public string Back()
+        {
+            return "";
+        }
+
+        /// <summary>
+        /// Шукає індекс в text з якого починається входження якогось s в цей текст
+        /// </summary>
+        /// <param name="text">Текст в якому відбувається пошук</param>
+        /// <param name="s"> стрічка яку шукають</param>
+        /// <returns> 
+        /// Якщо такого s немає в text повертається -1 
+        /// іначе повертається індекст з якого починається входження s
+        /// </returns>
+        /// <example>
+        /// Search("123","2") => 1
+        /// Search("123","1") => 0
+        /// Search("123","3") => 2
+        /// Search("123","23") => 1
+        /// Search("123","5") => -1
+        /// Search("121","1") => 0
+        /// </example>
+        public int Search(string text, string s)
+        {
+            
+            return 0;
+        }
+    }
+}
